11deac8c386eaf49da69adcb56b08a09dc539841
unknown <yuranykolysak@gmail.com>
2020-12-28 18:49:16 +0200

    fixed tests

--- tests.py
+++ tests.py
@@ -1,17 +1,20 @@
 import unittest
-from util import algorithm, re_algorightm
+from util import algorithm
 
 
 class AlgorithmTester(unittest.TestCase):
 
     def test_algorithm_on_first_data_example(self):
-        self.assertEqual(algorithm('in1.txt'), re_algorightm('in1.txt'))
+        self.assertEqual(algorithm('in1.txt'), [0, 1, 2, 3, 16, 17])
 
     def test_algorithm_on_second_data_example(self):
-        self.assertEqual(algorithm('in2.txt'), re_algorightm('in2.txt'))
+        self.assertEqual(algorithm('in2.txt'), [2, 4, 13, 16, 20])
 
     def test_algorithm_on_third_data_example(self):
-        self.assertEqual(algorithm('in3.txt'), re_algorightm('in3.txt'))
+        self.assertEqual(algorithm('in3.txt'), [14, 16, 22])
+
+    def test_algorithm_on_fourth_data_example(self):
+        self.assertEqual(algorithm('in4.txt'), [0, 7, 32])
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -2,28 +2,29 @@ import re
 
 
 def naive_substring_search(string, pattern_string):
-    occurrences = []
-
+    concurrences = []
     for i in range(len(string) - len(pattern_string) + 1):
         for j in range(len(pattern_string)):
             if string[i + j] != pattern_string[j]:
                 break
         else:
-            occurrences.append(i)
-    return occurrences
+            concurrences.append(i)
+    return concurrences
 
 
 def read_data(path):
     with open(path, 'r') as f:
-        string, pattern_string = tuple(map(str.strip, (path.readline(), path.readline())))
+        string, pattern_string = tuple(map(str.strip, (f.readline(), f.readline())))
     return string, pattern_string
 
 
 def algorithm(path):
-    return naive_substring_search(read_data(path))
+    data = read_data(path)
+    return naive_substring_search(data[0], data[1])
 
 
-def re_algorightm(path):
+def re_substings(path):
     substring = read_data(path)[1]
     string = read_data(path)[0]
-    ([match.span()[0] for match in tuple(re.finditer(substring, string))])
+    return [match.start() for match in tuple(re.finditer(substring, string))]
+
